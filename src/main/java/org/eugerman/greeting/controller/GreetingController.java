9aecefb5ed85f64d7ab82a76ce5e29c54ef60b6d
Yauheni Hermanovich <german.yauhen@icloud.com>
2025-11-08 22:14:02 +0100

    YH - Added test

--- src/main/java/org/eugerman/greeting/controller/GreetingController.java
+++ src/main/java/org/eugerman/greeting/controller/GreetingController.java
@@ -1,14 +1,20 @@
 package org.eugerman.greeting.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    @GetMapping
+    static final String GREETING_MSG_TEMPLATE = "Hello, %s, from dummy greeting application";
+    static final String GREET_ULR = "/greet";
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(GREET_ULR)
     public String greeting(@RequestParam("name") String name) {
-        return "Hello, %s, from dummy greeting application".formatted(name);
+        return GREETING_MSG_TEMPLATE.formatted(name);
     }
 }
